8bf8bfcfd5b94ec29f2d63c7b8a682e55e1935a4
Your Name <you@example.com>
2024-07-29 11:16:38 -0300

    Folder's Edit Modal Done!

--- Novaes-Web/js/AddFolderModal.js
+++ Novaes-Web/js/AddFolderModal.js
@@ -2,15 +2,16 @@ function showModal() {
     document.getElementById('AddFolderModal').style.display = 'block';
 }
 
-
 function hideModal() {
     document.getElementById('AddFolderModal').style.display = 'none';
 }
 
 function saveButton() {
-    console.log('Salvar');
+    console.log('Salvar Pasta');
     hideModal();
 }
+
+
 
 function showModalFile() {
     document.getElementById('AddFolderModalFile').style.display = 'block';
@@ -23,4 +24,19 @@ function hideModalFile() {
 function saveButtonFile() {
     console.log('Salvar arquivo');
     hideModalFile();
+}
+
+
+
+function showModalFolderName() {
+    document.getElementById('EditModalFolder').style.display = "block";
+}
+
+function hideModalEdit() {
+    document.getElementById('EditModalFolder').style.display = "none";
+}
+
+function saveButtonEditFolder () {
+    console.log('Salvar Edição Pasta!')
+    hideModalEdit();
 }
